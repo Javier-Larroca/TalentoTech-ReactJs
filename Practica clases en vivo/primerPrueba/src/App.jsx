5390c6cc06f06228ad992baca233acd01d4b41bf
Javier Larroca <javier-larroca@hotmail.com>
2025-09-16 14:34:14 -0300

    Practica realizada en clase 4

--- Practica clases en vivo/primerPrueba/src/App.jsx
+++ Practica clases en vivo/primerPrueba/src/App.jsx
@@ -1,28 +1,18 @@
 import './App.css'
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
-import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import Form from './components/Form/Form'; 
+import { Boton } from './components/Boton/Boton';
 
 function App() {
-  const arrayProductos = [
-    {id: 1, nombre: "Remera", precio: 45000, descripcion: "Remera de algodon"},
-    {id: 2, nombre: "Pantalon", precio: 89999, descripcion: "Pantalon de jean"},
-    {id: 3, nombre: "Buzo", precio: 2500, descripcion: "Buzo oversize"}
-  ];
-  //const prod = {nombre: "Remera", precio: 2500, descripcion: "lalalala"};
-
   return (
-    <>
-      <div>
-        <Header/>
-        <ItemListContainer 
-          titulo={"Bienvenidos a la tienda de Software"}
-          productos={arrayProductos}
-        />
-        <Footer/>
-      </div>
-    </>
+    <div>
+      <Header />
+      <Boton color={"green"} texto={"Saludar"} />
+      <Form />
+      <Footer />
+    </div>
   )
 }
 
-export default App
+export default App;
